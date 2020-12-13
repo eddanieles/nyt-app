e576c24a20c6f54fda586aa27be19bee54b6ff79
eddanieles <eddanieles@gmail.com>
2020-12-13 06:47:34 -0500

    Update Article.vue

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,16 +1,31 @@
 <template>
   <div>
     <Article />
-    <router-link :to="`/profile/favorites`">
-        <button v-on:click="saveToFavorites()">Save To Favorites</button>
-    </router-link>
+    <div v-if="showFavoriteButton">
+        <router-link :to="`/profile/favorites`">
+            <button v-if="showFavoriteButton" v-on:click="saveToFavorites()">Save To Favorites</button>
+        </router-link>
+    </div>
+    <div v-else>
+        <p class="saved">Saved on: {{savedDate}}</p>
+    </div>
   </div>
 </template>
 
 <script>
 import Article from '../components/Article'
+import { favoritesCollection } from '@/firebase'
+import { auth } from '../firebase'
+import _ from 'underscore'
+import moment from 'moment'
 
 export default {
+    data() {
+        return {
+            showFavoriteButton: true,
+            savedDate: ""
+        }
+    },
     components: {
         Article
     },
@@ -19,7 +34,25 @@ export default {
             this.$store.dispatch('createFavorite', {
                 articleId : this.$route.params.id
             })
+        },
+        async checkFavorites() {
+            const docs = await favoritesCollection.where('userId', '==', auth.currentUser.uid).get();
+            
+            const renderedArray = docs.docs.map(doc => {
+                let renderedObj = doc.data();
+                return renderedObj;
+            });
+
+            const foundFavorite = _.find(renderedArray, favorite => favorite.userId === auth.currentUser.uid && favorite.articleId === this.$route.params.id);
+
+            if(foundFavorite)  {
+                this.showFavoriteButton = !this.showFavoriteButton; 
+                this.savedDate = moment(foundFavorite.createdOn.toDate()).format("MM/DD/YYYY");
+            }
         }
+    },
+    beforeMount() {
+        this.checkFavorites();
     }
 }
 </script>
