cfadf833ace205d7c8898af5fd6d4025bee2098c
eddanieles <eddanieles@gmail.com>
2021-04-15 02:22:57 -0400

    deploy scripts

--- src/vue.config.js
+++ src/vue.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production' ?
+        '/nyt-app/' : '/',
     chainWebpack: config => {
         config.module.rules.delete("svg");
     },
